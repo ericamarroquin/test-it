b3d59e876092158c2ab3074ad4ba5c6a75b0a73a
EricaMarroquin <ericamarroquin03@gmail.com>
2021-08-31 16:25:38 -0700

    add passing test where tempScore is set to totalScore
    Co-authored-by: Melissa Schatz-Miller <melissa.scmi@gmail.com>
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- __test__/pigdice.test.js
+++ __test__/pigdice.test.js
@@ -1,44 +1,49 @@
-import { roll } from "./../src/pigdice.js";
 import { Player } from "./../src/pigdice.js";
-
-describe("roll", () => {
-
-  test("should display a random number between 1 and 6", () => {
-    const value = roll();
-    expect(value).toBeGreaterThanOrEqual(1);
-    expect(value).toBeLessThanOrEqual(6);
-  });
-});
 
 describe("Player", () => {
   
-    test("should create player object", () => {
-      let player = new Player();
-      expect(player).toBeDefined();
-    });
-
-    test("should create player object with start up properties", () => {
-      let player = new Player();
-      expect(player.tempScore).toEqual(0);
-      expect(player.totalScore).toEqual(0);
-      expect(player.currentPlayer).toEqual(true);
-    });
+  test("should create player object", () => {
+    let player = new Player();
+    expect(player).toBeDefined();
+  });
+  
+  test("should display a random number between 1 and 6", () => {
+    let player = new Player();
+    player.roll();
+    expect(player.currentRoll).toBeGreaterThanOrEqual(1);
+    expect(player.currentRoll).toBeLessThanOrEqual(6);
+  });
+  
+  test("should create player object with start up properties", () => {
+    let player = new Player();
+    expect(player.tempScore).toEqual(0);
+    expect(player.totalScore).toEqual(0);
+    expect(player.currentPlayer).toEqual(true);
+  });
     
-    test("should add roll to player temp score", () => {
-      let player = new Player();
-      player.play();
-      expect(player.tempScore).toBeGreaterThanOrEqual(1);
-      expect(player.tempScore).toBeLessThanOrEqual(6);
-    });
+  test("should add roll to player temp score", () => {
+    let player = new Player();
+    player.roll();
+    player.play();
+    expect(player.tempScore).toBeGreaterThanOrEqual(1);
+    expect(player.tempScore).toBeLessThanOrEqual(6);
+  });
 
-    test("toggle currentPlayer to false if rolled a 1", () => {
-      let player = new Player();
-      player.play();
-      expect(player.currentPlayer).toEqual(false);
-    });
+  test("toggle currentPlayer to false if rolled a 1", () => {
+    let player = new Player();
+    player.currentRoll = 1;
+    player.play();
+    expect(player.currentPlayer).toEqual(false);
+  });
+
+  test("should move tempScore to totalScore", () => {
+    let player = new Player();
+    player.roll();
+    player.play();
+    player.hold();
+    expect(player.totalScore).toEqual(player.tempScore);
+  });
 });
-
-
 
 
 
--- src/pigdice.js
+++ src/pigdice.js
@@ -1,19 +1,23 @@
-export function roll() {
-  const min = Math.ceil(1);
-  const max = Math.floor(7);
-  return Math.floor(Math.random() * (max-min) + min);
-};
-
 export function Player() {
   this.tempScore = 0;
   this.totalScore = 0;
   this.currentPlayer = true;
+  this.currentRoll = 0;
+};
+
+Player.prototype.roll = function() {
+  const min = Math.ceil(1);
+  const max = Math.floor(7);
+  this.currentRoll = Math.floor(Math.random() * (max-min) + min);
 };
 
 Player.prototype.play = function() {
-  const playerRoll = 1;
-  this.tempScore += playerRoll;
-  if (playerRoll === 1) {
+  this.tempScore += this.currentRoll;
+  if (this.currentRoll === 1) {
     this.currentPlayer = false;
   }; 
+};
+
+Player.prototype.hold = function() {
+this.totalScore += this.tempScore;
 };
